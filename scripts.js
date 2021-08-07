b41335977ab91441d5450b3914544bec34e4bba3
Luiz Henrique <luizhenrique50@live.com>
2021-08-07 18:16:56 -0300

    Adicionado função de mostrar e esconder comentários, junto da inserção dos comentários na tela

--- scripts.js
+++ scripts.js
@@ -1,31 +1,52 @@
+//like e deslike
 function clickHeart() {
-    let initialImg = document.getElementById("toggleHeart").src;
-    let srcTest = initialImg.includes('icones/coracao.png');
-    let newImg = {
+    let imagemInicial = document.getElementById("toggleHeart").src;
+    let srcTeste = imagemInicial.includes('icones/coracao.png');
+    let novaImagem = {
         'true': 'icones/coracao_red.png',
         'false': 'icones/coracao.png'
-    }[srcTest];
+    }[srcTeste];
     console.log("clicou")
-    return newImg;
+    return novaImagem;
 
 }
 toggleHeart.addEventListener("click", function() {
     document.getElementById("toggleHeart").src = clickHeart();
 })
 
+//duplo clique na foto 
 post.addEventListener("dblclick", function() {
     document.getElementById("toggleHeart").src = clickHeart();
     console.log("post")
 })
 
+//Mostrar area de comentarios
 commentBtn.addEventListener("click", function() {
     console.log("click comment")
-        /*   var x = document.getElementById("form");
-           if (x.style.display === "none") {
-               x.style.display = "block";
-           } else {
-               x.style.display = "none";
-           } */
-    document.getElementById("#formulario").style.visibility = "visible";
 
+    document.getElementById("form").style.visibility = "visible";
+
+})
+
+//Botão comentários
+btnComentar.addEventListener("click", function() {
+    let comentario = document.querySelector("#txtComentario");
+    let valorComentario = txtComentario.value
+
+    console.log(valorComentario)
+
+    if (valorComentario == '') {
+        alert("Digite um comentário!");
+        return;
+    }
+
+    areaComentarios.innerHTML += `
+    <div id="comment" class="comentario">
+    <span>Luiz</span>
+    <p>${valorComentario}</p>
+    </div>
+    `;
+    txtComentario.value = ""
+
+    document.getElementById("form").style.visibility = "hidden";
 })
